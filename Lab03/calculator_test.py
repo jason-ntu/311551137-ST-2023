6049a5dc98d526fbc74f79a2531bdda9e305a4d7
Jason <df41022@gmail.com>
2023-03-09 20:23:52 +0800

    Lab03 complete

--- Lab03/calculator_test.py
+++ Lab03/calculator_test.py
@@ -1,19 +1,34 @@
-import unittest
+from unittest import TestCase
 from calculator import Calculator
 
-class ApplicationTest(unittest.TestCase):
+
+class ApplicationTest(TestCase):
 
     def test_add(self):
-        pass
+        valid_params = {(0, 1): 1, (1, 2): 3, (0, -1): -1, (-1, -2): -3, (1, -1): 0}
+        for key, value in valid_params.items():
+            with self.subTest():
+                self.assertEqual(Calculator.add(key[0], key[1]), value)
+        self.assertRaises(TypeError, Calculator.add, 1, "")
 
     def test_divide(self):
-        pass
+        valid_params = {(0, 1): 0, (2, 1): 2, (3, 2): 1.5, (4, -1): -4, (0, -1): 0}
+        for key, value in valid_params.items():
+            with self.subTest():
+                self.assertEqual(Calculator.divide(key[0], key[1]), value)
+        self.assertRaises(ZeroDivisionError, Calculator.divide, 1, 0)
 
     def test_sqrt(self):
-        pass
+        valid_params = {0: 0, 1: 1, 4: 2, 121: 11, 169: 13}
+        for key, value in valid_params.items():
+            with self.subTest():
+                self.assertEqual(Calculator.sqrt(key), value)
+        self.assertRaises(ValueError, Calculator.sqrt, -1)
 
     def test_exp(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+        valid_params = {0: 1, 1: 2.718281828459045, 2: 7.38905609893065,
+                        -1: 0.36787944117144233, -2: 0.1353352832366127}
+        for key, value in valid_params.items():
+            with self.subTest():
+                self.assertEqual(Calculator.exp(key), value)
+        self.assertRaises(OverflowError, Calculator.exp, 1000)
--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 ![Lab01 workflow](https://github.com/jason-ntu/311551137-ST-2023/actions/workflows/Lab01-CI.yml/badge.svg)
 
 ![Lab02 workflow](https://github.com/jason-ntu/311551137-ST-2023/actions/workflows/Lab02-CI.yml/badge.svg)
+
+![Lab03 workflow](https://github.com/jason-ntu/311551137-ST-2023/actions/workflows/Lab03-CI.yml/badge.svg)
