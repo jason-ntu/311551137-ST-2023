a5c836b468c05cee2ef41c7977ad1d4266871f56
Jason <df41022@gmail.com>
2023-03-18 03:42:14 +0800

    refactor

--- Lab04/app.py
+++ Lab04/app.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,31 +7,33 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 myStudentID = "311551137"
+NYCU_URL = "https://www.nycu.edu.tw/"
+GOOGLE_URL = "https://www.google.com"
 
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--window-size=1920,1080')
 options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                           options=options)
-wait = WebDriverWait(driver, 10)
+# remove options if running locally
+# i.e. driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver.get("https://www.nycu.edu.tw/")
+wait = WebDriverWait(driver, 5)
+
+driver.get(NYCU_URL)
 
 driver.maximize_window()
 
 news = wait.until(EC.presence_of_element_located((By.LINK_TEXT, '新聞')))
 news.click()
 
-first_news = wait.until(EC.presence_of_element_located(
-    (By.XPATH, '//li[@class="su-post"]/a[1]')))
-first_news.click()
+all_news = wait.until(EC.presence_of_all_elements_located(
+    (By.CSS_SELECTOR, '.su-post > a')))
+all_news[0].click()
 
-title = wait.until(EC.presence_of_element_located((
-    By.XPATH, '//header[@class="entry-header clr"]/h1')))
-contents = wait.until(EC.presence_of_all_elements_located((
-    By.XPATH, '//div[@class="entry-content clr"]/p')))
+title = wait.until(EC.presence_of_element_located((By.TAG_NAME, 'h1')))
+contents = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'p')))
 print(title.text)
 for content in contents:
     print(content.text)
@@ -40,14 +41,14 @@ for content in contents:
 driver.switch_to.new_window('tab')
 wait.until(EC.new_window_is_opened)
 
-driver.get("https://www.google.com")
+driver.get(GOOGLE_URL)
 
 search_box = wait.until(EC.presence_of_element_located((By.NAME, 'q')))
 search_box.send_keys(myStudentID)
-search_box.send_keys(Keys.RETURN)
+search_box.submit()
 
-second_title = wait.until(EC.presence_of_element_located((
-    By.XPATH, '//div[@id="search"]/div/div/div[2]/div/div/div/div/a/h3')))
-print(second_title.text)
+all_titles = wait.until(EC.presence_of_all_elements_located(
+    (By.CLASS_NAME, 'LC20lb.MBeuO.DKV0Md')))
+print(all_titles[1].text)
 
 driver.close()
